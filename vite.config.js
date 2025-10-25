cccda12d38cb2ad3b525869eb07c98aa12859485
Thanmai56 <boyapati.thanmaisree@gmail.com>
2025-10-25 08:20:00 +0530

    changed cit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/carrental-frontend/",
+  base: "/s201-insem1-2300030103_Thanmai/",
   plugins: [react()],
 })
